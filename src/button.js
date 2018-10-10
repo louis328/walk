f1dfcd3f3f3d5179a907a4c126b5b0ab81fd73ea
louis328 <momonga654@gmail.com>
2018-10-10 22:11:04 +0900

    操作ボタン追加

---
+++ src/button.js
@@ -0,0 +1,23 @@
+import {gameObject} from './gameObject.js';
+import {canvas, CANVAS_WIDTH, CANVAS_HEIGHT} from './canvas.js';
+import {Polygon} from './polygon.js';
+
+export class ButtonController extends gameObject{
+    constructor(){
+        super("button");
+        this.buttonList = new Array();
+        console.log("yeah");
+        let buttonImage1 = new Polygon("button1", 1);
+        buttonImage1.setPosition(CANVAS_WIDTH-140, -140);
+        canvas.setTarget(buttonImage1);
+        this.buttonList.push(buttonImage1);
+        let buttonImage2 = new Polygon("button2", 1);
+        buttonImage2.setPosition(-CANVAS_WIDTH+400, -340);
+        canvas.setTarget(buttonImage2);
+        this.buttonList.push(buttonImage2);
+        let buttonImage3 = new Polygon("button3", 1);
+        buttonImage3.setPosition(-CANVAS_WIDTH+140, -340);
+        canvas.setTarget(buttonImage3);
+        this.buttonList.push(buttonImage3);
+    }
+}
--- src/game.js
+++ src/game.js
@@ -1,13 +1,14 @@
-import {canvas, CANVAS_WIDTH, CANVAS_HEIGHT} from './canvas.js';
+import {canvas, CANVAS_WIDTH, CANVAS_HEIGHT, PC_MODE} from './canvas.js';
 import {messenger} from './messaenger.js';
 import {Hiyoko} from './hiyoko.js';
 import {Blocks} from './blocks.js';
 import {keyManager} from './keyManager.js';
+import { ButtonController } from './button.js';
 
 onload = function(){
 
   document.documentElement.addEventListener('touchstart', function (e) {
-    let x = e.changedTouches[0].pageX;
+    let x = e.changedTouches[0].pageX + CANVAS_WIDTH/2;
     let y = CANVAS_HEIGHT/2 - e.changedTouches[0].pageY;
     let message = new Object();
     message['x'] = x;
@@ -22,6 +23,10 @@ onload = function(){
   
   let hiyoko = new Hiyoko();
   let blocks = new Blocks();
+  let buttonController  = null;
+  if(!PC_MODE){
+    buttonController = new ButtonController();
+  }
 
   (function func (){
     keyManager.process();
