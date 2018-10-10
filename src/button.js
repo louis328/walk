f3c589e8deb6019cac4a7bcb8d62d61051a4276c
louis328 <momonga654@gmail.com>
2018-10-10 22:15:16 +0900

    操作ボタン位置調整

--- src/button.js
+++ src/button.js
@@ -8,15 +8,15 @@ export class ButtonController extends gameObject{
         this.buttonList = new Array();
         console.log("yeah");
         let buttonImage1 = new Polygon("button1", 1);
-        buttonImage1.setPosition(CANVAS_WIDTH-140, -140);
+        buttonImage1.setPosition(CANVAS_WIDTH/2-140, -240);
         canvas.setTarget(buttonImage1);
         this.buttonList.push(buttonImage1);
         let buttonImage2 = new Polygon("button2", 1);
-        buttonImage2.setPosition(-CANVAS_WIDTH+400, -340);
+        buttonImage2.setPosition(-CANVAS_WIDTH/2+400, -440);
         canvas.setTarget(buttonImage2);
         this.buttonList.push(buttonImage2);
         let buttonImage3 = new Polygon("button3", 1);
-        buttonImage3.setPosition(-CANVAS_WIDTH+140, -340);
+        buttonImage3.setPosition(-CANVAS_WIDTH/2+140, -440);
         canvas.setTarget(buttonImage3);
         this.buttonList.push(buttonImage3);
     }
